7810e7ea2d4c83a6245f75e9b2cfeb1331adab04
ZombineDev <petar.p.kirov@gmail.com>
2016-11-10 21:04:55 +0200

    Implement MongoDB data retrieval
    And cleanup the code:
    * Remove unnecessary test code
    * Reorder function declarations to make the module more clear on first look
    * Use selective imports
    * Use const where possible
    * Cleanup whitespace

--- source/app.d
+++ source/app.d
@@ -1,50 +1,62 @@
-import vibe.core.core;
-import vibe.core.log;
-import vibe.http.router;
-import vibe.http.server;
-import vibe.web.rest;
-import vibe.http.fileserver;
+import vibe.http.router : URLRouter;
+import vibe.http.fileserver : serveStaticFiles;
+import vibe.http.server : HTTPServerSettings, listenHTTP;
+import vibe.web.rest : registerRestInterface;
+import vibe.db.mongo.mongo : connectMongoDB;
+import vibe.data.bson : deserializeBson;
+import std.format : format;
+import std.algorithm.comparison : min;
 
 interface ILoremSongAPI
 {
-	string getSong(string theme, int count);
-}
-
-class LoremSongAPI : ILoremSongAPI
-{
-	string getSong(string theme, int count)
-    {
-        import std.format : format;
-        return format("Requested: theme=%s, count=%s",
-            theme, count);
-    }
+    string getSong(string theme, int count);
 }
 
 shared static this()
 {
-	import std.getopt : getopt;
-	import core.runtime : Runtime;
-	auto args = Runtime.args;
-	bool test;
- 	getopt(args, "test", &test);
+    auto router = new URLRouter;
+    router
+        .get("*", serveStaticFiles("./public/"))
+        .registerRestInterface(new LoremSongAPI);
 
-	auto router = new URLRouter;
-	router.registerRestInterface(new LoremSongAPI);
-	router.get("*", serveStaticFiles("./public/"));
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+    auto settings = new HTTPServerSettings();
+    settings.port = 8080;
 
-	if (test)
-		settings.bindAddresses = ["::1", "127.0.0.1", "10.23.6.219"];
-	else
-		settings.bindAddresses = ["::1", "127.0.0.1"];
+    if (useLocalIpForTesting)
+        settings.bindAddresses = ["::1", "127.0.0.1", "10.23.6.219"];
+    else
+        settings.bindAddresses = ["::1", "127.0.0.1"];
 
-	listenHTTP(settings, router);
+    listenHTTP(settings, router);
+}
 
-	// create a client to talk to the API implementation over the REST interface
-	runTask({
-		auto client = new RestInterfaceClient!ILoremSongAPI("http://127.0.0.1:8080/");
-		auto song = client.getSong("love", 3);
-		logInfo("Song: %s", song);
-	});
+class LoremSongAPI : ILoremSongAPI
+{
+    string getSong(string theme, int count)
+    {
+        auto client = connectMongoDB("127.0.0.1", 27017);
+        auto songs = client.getCollection("lorem.songs");
+        auto song = songs.findOne(["theme" : ["$eq" : theme]]);
+        auto verses = song["data"].deserializeBson!(string[]);
+
+        string[] result;
+
+        foreach (i; 0 .. min(count, verses.length))
+            result ~= "<div>%s</div>".format(verses[i]);
+
+        return "%-(%s\n%)".format(result);
+    }
+}
+
+bool useLocalIpForTesting()
+{
+    import std.getopt : getopt;
+    import core.runtime : Runtime;
+
+    auto args = Runtime.args;
+
+    bool test;
+    getopt(args, "test", &test);
+
+    return test;
 }
