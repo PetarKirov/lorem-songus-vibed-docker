1cb277151ff994cec2531902812045ff23175e7b
ZombineDev <petar.p.kirov@gmail.com>
2016-11-10 18:06:13 +0200

    Add dummy REST API implementation

--- source/app.d
+++ source/app.d
@@ -1,16 +1,50 @@
-import vibe.d;
+import vibe.core.core;
+import vibe.core.log;
+import vibe.http.router;
+import vibe.http.server;
+import vibe.web.rest;
+import vibe.http.fileserver;
+
+interface ILoremSongAPI
+{
+	string getSong(string theme, int count);
+}
+
+class LoremSongAPI : ILoremSongAPI
+{
+	string getSong(string theme, int count)
+    {
+        import std.format : format;
+        return format("Requested: theme=%s, count=%s",
+            theme, count);
+    }
+}
 
 shared static this()
 {
+	import std.getopt : getopt;
+	import core.runtime : Runtime;
+	auto args = Runtime.args;
+	bool test;
+ 	getopt(args, "test", &test);
+
+	auto router = new URLRouter;
+	router.registerRestInterface(new LoremSongAPI);
+	router.get("*", serveStaticFiles("./public/"));
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1", "10.23.6.219"];
-	listenHTTP(settings, &hello);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-}
+	if (test)
+		settings.bindAddresses = ["::1", "127.0.0.1", "10.23.6.219"];
+	else
+		settings.bindAddresses = ["::1", "127.0.0.1"];
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
+	listenHTTP(settings, router);
+
+	// create a client to talk to the API implementation over the REST interface
+	runTask({
+		auto client = new RestInterfaceClient!ILoremSongAPI("http://127.0.0.1:8080/");
+		auto song = client.getSong("love", 3);
+		logInfo("Song: %s", song);
+	});
 }
